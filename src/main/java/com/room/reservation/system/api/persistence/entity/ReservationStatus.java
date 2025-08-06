06759414a94d3c54cd339b65c0c94d0f5e488e8b
CHANG MIN PARK <98qkrckdals@naver.com>
2025-08-07 06:19:40 +0900

    refactor: 필요 없는 필드 제거

--- src/main/java/com/room/reservation/system/api/persistence/entity/ReservationStatus.java
+++ src/main/java/com/room/reservation/system/api/persistence/entity/ReservationStatus.java
@@ -2,7 +2,5 @@ package com.room.reservation.system.api.persistence.entity;
 
 public enum ReservationStatus {
     PENDING,    // 결제 대기
-    CONFIRMED,  // 예약 확정
-    CANCELLED,  // 예약 취소
-    COMPLETED   // 예약 완료
+    CONFIRMED   // 예약 확정 (결제 완료)
 } 
